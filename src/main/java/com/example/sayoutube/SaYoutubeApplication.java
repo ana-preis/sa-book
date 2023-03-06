ac23bc5c69495e656476a8932ed603dd635ce413
ana-preis <anarps_@hotmail.com>
2023-03-06 20:46:43 -0300

    initial scaffolding

---
+++ src/main/java/com/example/sayoutube/SaYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sayoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaYoutubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sayoutube/SaYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sayoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
